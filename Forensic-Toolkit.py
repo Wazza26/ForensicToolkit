de7722767ded65035fcb3b70892e42eda6265f78
Wazza26 <James.wasley@blueyonder.co.uk>
2016-12-22 14:28:38 +0000

    Update Forensic-Toolkit.py

--- Forensic-Toolkit.py
+++ Forensic-Toolkit.py
@@ -3,6 +3,7 @@
 import subprocess
 import argparse
 import os
+from pathlib import Path
 
 #Introduction to the program. First function to run. Used to gather the users' options
 def launchScreen():
@@ -16,20 +17,20 @@ def vmwareCLIargparse():
     parser = argparse.ArgumentParser(description="VMWare CLI required arguements")
     parser.add_argument("filepath", help="Enter file path of .VMX")
     args = parser.parse_args()
-    return args.filepath
+    
+    return Path(args.filepath)
 
 #Pushes directory on CMD to point where vmrun executable is
-def pushd():
-    previous_dir = os.getcwd()
+def cd():
     vmPath = "C:\Program Files (x86)\\VMware\\VMware Workstation\\"
     os.chdir(vmPath)
 
 #Launching the virtual machine via VMWare CLI
 def executingVM(filepath):
-    pushd()
+    cd()
     print ("Launching Virtual Machine....")
     print (filepath)
-    subprocess.call("vmrun start", shell="True", args=["\"" + filepath + "\""])
+    subprocess.call(["vmrun", "start", "\"" + filepath + "\""], shell="True")
     
 #def generatingSnapshot():
 #   print ("Launching Virtual Machine....")
